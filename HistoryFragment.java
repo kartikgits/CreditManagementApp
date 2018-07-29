c9b0044fdda287f39f13b84220ef7125389e22b0
kartikgits <kartik.kapparwan1@gmail.com>
2018-07-29 08:55:31 +0530

    Setup Raw Fragments

---
+++ HistoryFragment.java
@@ -0,0 +1,31 @@
+package com.northwindlabs.kartikeya.creditmanagement;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.special.ResideMenu.ResideMenu;
+
+public class HistoryFragment extends Fragment {
+    private View parentView;
+    private ResideMenu resideMenu;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        parentView = inflater.inflate(R.layout.fragment_history, container, false);
+        setUpViews();
+        return parentView;
+    }
+
+    private void setUpViews() {
+        MainActivity parentActivity = (MainActivity) getActivity();
+        resideMenu = parentActivity.getResideMenu();
+
+        // add gesture operation's ignored views
+        FrameLayout ignored_view = (FrameLayout) parentView.findViewById(R.id.ignored_view);
+        resideMenu.addIgnoredView(ignored_view);
+    }
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,115 @@
 package com.northwindlabs.kartikeya.creditmanagement;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
 
+public class MainActivity extends FragmentActivity implements View.OnClickListener{
+
+    private ResideMenu resideMenu;
+    private MainActivity mContext;
+    private ResideMenuItem itemUsers;
+    private ResideMenuItem itemHistory;
+    private ResideMenuItem itemAbout;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+        setUpMenu();
+        if( savedInstanceState == null )
+            changeFragment(new UserFragment());
+    }
+
+    private void setUpMenu() {
+
+        // attach to current activity;
+        resideMenu = new ResideMenu(this);
+        resideMenu.setBackground(R.drawable.menu_background);
+        resideMenu.attachToActivity(this);
+        //resideMenu.setMenuListener(menuListener);
+
+        //valid scale factor is between 0.0f and 1.0f. leftmenu'width is 150dip.
+        resideMenu.setScaleValue(0.6f);
+
+        // create menu items;
+        itemUsers     = new ResideMenuItem(this, R.drawable.ic_person_24dp,     R.string.menu_item_users_title);
+        itemHistory  = new ResideMenuItem(this, R.drawable.ic_history_24dp,  R.string.menu_item_history_title);
+        itemAbout = new ResideMenuItem(this, R.drawable.ic_about_24dp, R.string.menu_item_about_title);
+
+        itemUsers.setOnClickListener(this);
+        itemHistory.setOnClickListener(this);
+        itemAbout.setOnClickListener(this);
+
+        resideMenu.addMenuItem(itemUsers, ResideMenu.DIRECTION_LEFT);
+        resideMenu.addMenuItem(itemHistory, ResideMenu.DIRECTION_LEFT);
+        resideMenu.addMenuItem(itemAbout, ResideMenu.DIRECTION_LEFT);
+
+        // Disabling right swipe, we don't need this
+        resideMenu.setSwipeDirectionDisable(ResideMenu.DIRECTION_RIGHT);
+
+        findViewById(R.id.title_bar_left_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resideMenu.openMenu(ResideMenu.DIRECTION_LEFT);
+            }
+        });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return resideMenu.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == itemUsers){
+            changeFragment(new UserFragment());
+        }else if (view == itemHistory){
+            changeFragment(new HistoryFragment());
+        }else if (view == itemAbout){
+            changeFragment(new AboutFragment());
+        }
+
+        resideMenu.closeMenu();
+    }
+
+//    private ResideMenu.OnMenuListener menuListener = new ResideMenu.OnMenuListener() {
+//        @Override
+//        public void openMenu() {
+//            Toast.makeText(mContext, "Menu is opened!", Toast.LENGTH_SHORT).show();
+//        }
+//
+//        @Override
+//        public void closeMenu() {
+//            Toast.makeText(mContext, "Menu is closed!", Toast.LENGTH_SHORT).show();
+//        }
+//    };
+
+    private void changeFragment(Fragment targetFragment){
+        resideMenu.clearIgnoredViewList();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment, targetFragment, "fragment")
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
+
+    // What good method is to access resideMenu？
+    public ResideMenu getResideMenu(){
+        return resideMenu;
     }
 }
